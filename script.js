ec1fc89a578a24e71e9ce7c968a762eb963ab54d
Hernan Di Tano <hditano@gmail.com>
2021-03-24 03:52:01 -0300

    fixed while loop

--- script.js
+++ script.js
@@ -1,50 +1,52 @@
-let userName = "";
 let chooseRule = "";
 let rounds = 1;
 let userScore = 0;
 let computerScore = 0;
-let username = prompt("What's your Name?");
+let userName = prompt('Please type your name!');
 
-while (userScore <= 5 && computerScore <= 5) {
-  chooseRule = prompt("Type []Rock []Paper or []Scissors");
+function playRPS() {
+  while (userScore < 5 && computerScore < 5) {
+    chooseRule = prompt(`Hello ${userName}!! Please type []Rock []Paper or []Scissors`);
 
-  console.log(`Game Round ${rounds}`);
-  console.log(`User Score: ${userScore} Computer Score: ${computerScore}`)
-  function playRound(chooseRule, computerSelection) {
-    if (
-      (chooseRule === "Rock" && computerSelection === "Rock") ||
-      (chooseRule === "Scissors" && computerSelection === "Scissors") ||
-      (chooseRule === "Paper" && computerSelection === "Paper")
-    ) {
-      userScore++;
-      computerScore++;
-      return console.log(chooseRule, computerSelection, "You are Tied!!");
-    } else if (
-      (chooseRule === "Rock" && computerSelection === "Scissors") ||
-      (chooseRule === "Scissors" && computerSelection === "Paper") ||
-      (chooseRule === "Paper" && computerSelection === "Rock")
-    ) {
-      userScore++;
-      return console.log(chooseRule, computerSelection, "You Won!!");
-    } else {
-      computerScore++;
-      return console.log(chooseRule, computerSelection, "You Lost!!");
+    console.log(`Game Round ${rounds}`);
+    console.log(`User Score: ${userScore} Computer Score: ${computerScore}`);
+    function playRound(chooseRule, computerSelection) {
+      if (
+        (chooseRule === "Rock" && computerSelection === "Rock") ||
+        (chooseRule === "Scissors" && computerSelection === "Scissors") ||
+        (chooseRule === "Paper" && computerSelection === "Paper")
+      ) {
+        return console.log(chooseRule, computerSelection, "You are Tied!!");
+      } else if (
+        (chooseRule === "Rock" && computerSelection === "Scissors") ||
+        (chooseRule === "Scissors" && computerSelection === "Paper") ||
+        (chooseRule === "Paper" && computerSelection === "Rock")
+      ) {
+        userScore++;
+        return console.log(chooseRule, computerSelection, "You Won!!");
+      } else {
+        computerScore++;
+        return console.log(chooseRule, computerSelection, "You Lost!!");
+      }
     }
-  }
-  const computerSelection = playComputer();
+    const computerSelection = playComputer();
 
-  function playComputer() {
-    const rules = ["Rock", "Paper", "Scissors"];
-    let result = Math.floor(Math.random() * 3 );
-    return rules[result];
+    function playComputer() {
+      const rules = ["Rock", "Paper", "Scissors"];
+      let result = Math.floor(Math.random() * 3);
+      return rules[result];
+    }
+
+    playRound(chooseRule, computerSelection);
+    rounds++;
   }
 
-  playRound(chooseRule, computerSelection);
-  rounds++;
+  if (userScore >= 5 && computerScore < 5) {
+    console.log(`${userName} Won!`);
+    console.log(`User Score: ${userScore} Computer Score: ${computerScore}`);
+  } else if(userScore < 5 && computerScore >= 5) {
+    console.log("Computer Won!");
+  }
 }
 
-if(userScore === 5) {
-  console.log(`${userName} Won!`);
-} else {
-  console.log('Computer Won!');
-}
+playRPS();
