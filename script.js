b962be21ec7844bdd584578968ab4128fd81e014
Hernan Di Tano <hditano@gmail.com>
2021-03-24 00:37:56 -0300

    added while loop

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
 let userName = "";
 let chooseRule = "";
 let rounds = 1;
+let userScore = 0;
+let computerScore = 0;
 // username = prompt("What's your Name?");
 
-while (rounds <= 5) {
+while (userScore <= 5 && computerScore <= 5) {
   chooseRule = prompt("Type []Rock []Paper or []Scissors");
 
   console.log(`Game Round ${rounds}`);
-
+  console.log(`User Score: ${userScore} Computer Score: ${computerScore}`)
   function playRound(chooseRule, computerSelection) {
     if (
       (chooseRule === "Rock" && computerSelection === "Rock") ||
       (chooseRule === "Scissors" && computerSelection === "Scissors") ||
       (chooseRule === "Paper" && computerSelection === "Paper")
     ) {
+      userScore++;
+      computerScore++;
       return console.log(chooseRule, computerSelection, "You are Tied!!");
     } else if (
       (chooseRule === "Rock" && computerSelection === "Scissors") ||
       (chooseRule === "Scissors" && computerSelection === "Paper") ||
       (chooseRule === "Paper" && computerSelection === "Rock")
     ) {
+      userScore++;
       return console.log(chooseRule, computerSelection, "You Won!!");
     } else {
+      computerScore++;
       return console.log(chooseRule, computerSelection, "You Lost!!");
     }
   }
