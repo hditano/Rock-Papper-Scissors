9fcd7121e9bba3fc080c3ee42619798908c64766
Hernan Di Tano <hditano@gmail.com>
2021-03-31 02:19:38 -0300

    added logic to the game, deleted start game button, fixed small stuff

--- script.js
+++ script.js
@@ -2,11 +2,13 @@
 // let rounds = 1;
 // let userScore = 0;
 // let computerScore = 0;
-// let userName = prompt('Please type your name!');
+// let userName = prompt("Please type your name!");
 
 // function playRPS() {
 //   while (userScore < 5 && computerScore < 5) {
-//     chooseRule = prompt(`Hello ${userName}!! Please type []Rock []Paper or []Scissors`);
+//     chooseRule = prompt(
+//       `Hello ${userName}!! Please type []Rock []Paper or []Scissors`
+//     );
 
 //     console.log(`Game Round ${rounds}`);
 //     console.log(`User Score: ${userScore} Computer Score: ${computerScore}`);
@@ -44,7 +46,7 @@
 //   if (userScore >= 5 && computerScore < 5) {
 //     console.log(`${userName} Won!`);
 //     console.log(`User Score: ${userScore} Computer Score: ${computerScore}`);
-//   } else if(userScore < 5 && computerScore >= 5) {
+//   } else if (userScore < 5 && computerScore >= 5) {
 //     console.log("Computer Won!");
 //   }
 // }
@@ -54,28 +56,87 @@
 const submitBtn = document.querySelector(".submit-name");
 const textName = document.querySelector(".text-name");
 const playerName = document.querySelector(".player-name");
-const nameContainer = document.querySelector('.name-container');
+const nameContainer = document.querySelector(".name-container");
+const startRoundBtn = document.querySelector(".start-game");
+const gameButtons = document.querySelector('.play-btn');
+const rockBtn = document.querySelector(".rock-btn");
+const paperBtn = document.querySelector(".paper-btn");
+const scissorsBtn = document.querySelector(".scissors-btn");
+const playerCounter = document.querySelector(".player-counter");
+const computerCounter = document.querySelector(".computer-counter");
+const logs = document.querySelector(".logs");
+let userScore = 0;
+let computerScore = 0;
 
-submitBtn.addEventListener('click', (e) => {
-  console.log(e);
+submitBtn.addEventListener("click", () => {
   if (textName !== "") {
     changeText(textName, playerName);
-    document.querySelector('.name-container').style.visibility = 'hidden';
+    document.querySelector(".name-container").style.visibility = "hidden";
+    gameButtons.style.visibility = 'visible';
     startGame();
   }
 });
 
+
+rockBtn.addEventListener("click", (e) => {
+  playRound(e.target.innerText, playComputer());
+});
+
+paperBtn.addEventListener("click", (e) => {
+  playRound(e.target.innerText, playComputer());
+});
+
+scissorsBtn.addEventListener("click", (e) => {
+  playRound(e.target.innerText, playComputer());
+});
+
 // Universal function to changeText
 function changeText(origin, destination) {
-  return destination.textContent = origin.value;
+  return (destination.textContent = origin.value);
 }
 
 function startGame() {
-  const startButton = document.createElement('div');
-  startButton.setAttribute('class', 'main-startGame');
-  startButton.innerHTML = "<a href='#' class='btn-class start-game'>Start Game</a> <a href='#' class='btn-class reset-game'>Reset Game</a>";
-  startButton.style.visibility = 'visible';
-  startButton.style.textDecoration = 'none';
+  const startButton = document.createElement("div");
+  startButton.setAttribute("class", "main-startGame");
+  startButton.innerHTML =
+    "<a href='#' class='btn-class reset-game'>Reset Game</a>";
+  startButton.style.visibility = "visible";
+  startButton.style.textDecoration = "none";
   document.body.appendChild(startButton);
+}
 
+// function updateCounter(counter) {
+//   console.log(counter);
+// }
+
+function startRound() {
+  playComputer();
+}
+
+function playComputer() {
+  const rules = ["Rock", "Paper", "Scissors"];
+  let result = Math.floor(Math.random() * 3);
+  return rules[result];
+}
+
+function playRound(playerSelection, computerSelection) {
+  if (
+    (playerSelection === "Rock" && computerSelection === "Rock") ||
+    (playerSelection === "Scissors" && computerSelection === "Scissors") ||
+    (playerSelection === "Paper" && computerSelection === "Paper")
+  ) {
+    return (logs.textContent = ` Player: ${playerSelection} *** Computer: ${computerSelection} *** Result = Tie`);
+  } else if (
+    (playerSelection === "Rock" && computerSelection === "Scissors") ||
+    (playerSelection === "Scissors" && computerSelection === "Paper") ||
+    (playerSelection === "Paper" && computerSelection === "Rock")
+  ) {
+    userScore++;
+    playerCounter.textContent = userScore;
+    logs.textContent = ` Player: ${playerSelection} *** Computer: ${computerSelection} *** Result = You Won`;
+  } else {
+    computerScore++;
+    computerCounter.textContent = computerScore;
+    logs.textContent = ` Player: ${playerSelection} *** Computer: ${computerSelection} *** Result = You Lost`;
+  }
 }
